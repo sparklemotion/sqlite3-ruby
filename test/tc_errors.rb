d7369f59017b395d3da66c351388f94f5b23bf3c
Rob Holland <rob@inversepath.com>
2008-05-15 22:36:36 +0100

    switch to mocha

--- test/tc_errors.rb
+++ test/tc_errors.rb
@@ -2,13 +2,12 @@ $:.unshift "../lib"
 
 require 'sqlite3/errors'
 require 'test/unit'
-require 'flexmock'
+require 'mocha'
 
 class TC_Errors < Test::Unit::TestCase
   (1..26).each do |code|
     define_method( "test_error_code_%02d" % code ) do
-      db = FlexMock.new
-      db.mock_handle( :errmsg ) { "message" }
+      db = stub('database', :errmsg => 'message')
       begin
         SQLite3::Error.check( code, db )
       rescue SQLite3::Exception => e
