1a26f7257e28f35ae484bda42fce79d2ead7bcef
Aaron Patterson <aaron.patterson@gmail.com>
2010-05-11 16:47:35 -0700

    do not need this test in the extconf

--- ext/sqlite3/extconf.rb
+++ ext/sqlite3/extconf.rb
@@ -30,7 +30,6 @@ asplode('sqlite3') unless find_library 'sqlite3', 'sqlite3_libversion_number'
 
 # Functions defined in 1.9 but not 1.8
 have_func('rb_proc_arity')
-have_func('rb_obj_method_arity')
 
 # These functions may not be defined
 have_func('sqlite3_column_database_name')
