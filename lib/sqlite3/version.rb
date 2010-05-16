efbfc03b6ec71121e1fe494c2ada77f63b50a6bd
Luis Lavena <luislavena@gmail.com>
2010-05-15 22:03:16 -0300

    Bump version file

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -5,12 +5,12 @@ module SQLite3
     MAJOR = 1
     MINOR = 3
     TINY  = 0
-    BUILD = "beta.1"
+    BUILD = "beta.2"
 
     STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
     #:beta-tag:
 
-    VERSION = '1.3.0.beta.1'
+    VERSION = '1.3.0.beta.2'
   end
 
 end
