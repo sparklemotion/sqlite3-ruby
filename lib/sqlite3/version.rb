038a67e9391e09ebb8681a0c1e9b7b1cf2aa53c3
Luis Lavena <luislavena@gmail.com>
2010-07-09 23:58:34 -0300

    Preparation for 1.3.1 release.

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -4,13 +4,13 @@ module SQLite3
 
     MAJOR = 1
     MINOR = 3
-    TINY  = 0
+    TINY  = 1
     BUILD = nil
 
     STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
     #:beta-tag:
 
-    VERSION = '1.3.0'
+    VERSION = '1.3.1'
   end
 
 end
