70b9d17d53c3eefdfaf15d846c021b519aca33aa
Aaron Patterson <aaron.patterson@gmail.com>
2012-03-03 18:44:38 -0800

    use require rather than expanding paths so that the helper is required once

--- test/test_backup.rb
+++ test/test_backup.rb
@@ -1,4 +1,4 @@
-require File.expand_path('helper', File.dirname(__FILE__))
+require 'helper'
 
 module SQLite3
   class TestBackup < Test::Unit::TestCase
--- test/test_integration_statement.rb
+++ test/test_integration_statement.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), 'helper')
+require 'helper'
 
 class TC_Statement < Test::Unit::TestCase
   def setup
