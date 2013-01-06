85f0091d7d9b9f8b25b066fbcc8c76c2e6aaa897
Luis Lavena <luislavena@gmail.com>
2013-01-06 14:21:14 -0300

    Prepare for release

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -1,12 +1,12 @@
 module SQLite3
 
-  VERSION = '1.3.6'
+  VERSION = '1.3.7'
 
   module VersionProxy
 
     MAJOR = 1
     MINOR = 3
-    TINY  = 6
+    TINY  = 7
     BUILD = nil
 
     STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
