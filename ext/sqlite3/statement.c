473ae617aebf213da4a38980aa735d44a394a292
Nobuyoshi Nakada <nobu@ruby-lang.org>
2014-02-17 11:26:26 +0900

    Remove unused variable
    Remove unused variable, enc_index is no longer used since f9005a20.

--- ext/sqlite3/statement.c
+++ ext/sqlite3/statement.c
@@ -112,7 +112,6 @@ static VALUE step(VALUE self)
   VALUE list;
 #ifdef HAVE_RUBY_ENCODING_H
   rb_encoding * internal_encoding;
-  int enc_index;
 #endif
 
   Data_Get_Struct(self, sqlite3StmtRuby, ctx);
@@ -124,8 +123,7 @@ static VALUE step(VALUE self)
 #ifdef HAVE_RUBY_ENCODING_H
   {
       VALUE db          = rb_iv_get(self, "@connection");
-      VALUE encoding    = rb_funcall(db, rb_intern("encoding"), 0);
-      enc_index = NIL_P(encoding) ? rb_utf8_encindex() : rb_to_encoding_index(encoding);
+      rb_funcall(db, rb_intern("encoding"), 0);
       internal_encoding = rb_default_internal_encoding();
   }
 #endif
