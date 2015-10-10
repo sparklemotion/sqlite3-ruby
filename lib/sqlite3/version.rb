64250220fe55c4d4d03bb041c4e3818a891ab588
Luis Lavena <luislavena@gmail.com>
2015-10-10 17:12:37 -0300

    Backport: release 1.3.10
    Backport release changes to be up-to-date with 1-3-stable.

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -1,12 +1,12 @@
 module SQLite3
 
-  VERSION = '1.3.9'
+  VERSION = '1.3.10'
 
   module VersionProxy
 
     MAJOR = 1
     MINOR = 3
-    TINY  = 9
+    TINY  = 10
     BUILD = nil
 
     STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
