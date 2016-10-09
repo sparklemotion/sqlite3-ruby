f7e0b82aad4b6fd81a5a86cc8f38c824520270fe
Aaron Patterson <aaron.patterson@gmail.com>
2016-10-08 18:01:39 -0700

    bumping version

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -1,12 +1,12 @@
 module SQLite3
 
-  VERSION = '1.3.11'
+  VERSION = '1.3.12'
 
   module VersionProxy
 
     MAJOR = 1
     MINOR = 3
-    TINY  = 11
+    TINY  = 12
     BUILD = nil
 
     STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
