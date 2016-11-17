dc9e11c99aaa862a18588ea38190c59e2e50af57
Aaron Patterson <aaron.patterson@gmail.com>
2016-11-17 15:03:50 -0800

    test

--- test/test_database.rb
+++ test/test_database.rb
@@ -19,7 +19,7 @@ module SQLite3
       assert_equal '', @db.filename('main')
       tf = Tempfile.new 'thing'
       @db = SQLite3::Database.new tf.path
-      assert_equal tf.path, @db.filename('main')
+      assert_equal tf.path, Pathname.new(@db.filename('main')).to_s
     ensure
       tf.unlink if tf
     end
