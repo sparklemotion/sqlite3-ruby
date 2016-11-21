c86e345094eac19dfab1f41e388f70f404a75a9c
Aaron Patterson <aaron.patterson@gmail.com>
2016-11-21 13:34:37 -0800

    hoinaehtoin

--- test/test_database.rb
+++ test/test_database.rb
@@ -20,7 +20,7 @@ module SQLite3
       assert_equal '', @db.filename('main')
       tf = Tempfile.new 'thing'
       @db = SQLite3::Database.new tf.path
-      assert_equal File.expand_path(tf.path), File.expand_path(Pathname.new(@db.filename('main')).to_s)
+      assert_equal File.expand_path(tf.path), File.expand_path(@db.filename('main'))
     ensure
       tf.unlink if tf
     end
@@ -30,7 +30,7 @@ module SQLite3
       assert_equal '', @db.filename
       tf = Tempfile.new 'thing'
       @db = SQLite3::Database.new tf.path
-      assert_equal tf.path, @db.filename
+      assert_equal File.expand_path(tf.path), File.expand_path(@db.filename)
     ensure
       tf.unlink if tf
     end
@@ -40,7 +40,7 @@ module SQLite3
       assert_equal '', @db.filename
       tf = Tempfile.new 'thing'
       @db.execute "ATTACH DATABASE '#{tf.path}' AS 'testing'"
-      assert_equal tf.path, @db.filename('testing')
+      assert_equal File.expand_path(tf.path), File.expand_path(@db.filename('testing'))
     ensure
       tf.unlink if tf
     end
