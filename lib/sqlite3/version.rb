ee6bf89919418f1b8625f7b5af2ba22cb05a8836
Mike Dalessio <mike.dalessio@gmail.com>
2023-01-04 17:19:49 -0500

    version bump to 1.6.0.rc2

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -1,12 +1,12 @@
 module SQLite3
 
-  VERSION = "1.6.0.rc1"
+  VERSION = "1.6.0.rc2"
 
   module VersionProxy
     MAJOR = 1
     MINOR = 6
     TINY  = 0
-    BUILD = "rc1"
+    BUILD = "rc2"
 
     STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
 
