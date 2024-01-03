40e7b290079ca9f0b31917ee4afc5d9f0ed16642
Mike Dalessio <mike.dalessio@gmail.com>
2024-01-03 13:45:44 -0500

    remove: SQLite3::VersionProxy
    deprecated in v1.3.2 (circa 2010)

--- lib/sqlite3/version.rb
+++ lib/sqlite3/version.rb
@@ -1,23 +1,3 @@
 module SQLite3
-
   VERSION = "2.0.0.dev"
-
-  module VersionProxy
-    MAJOR = 2
-    MINOR = 0
-    TINY  = 0
-    BUILD = "dev"
-
-    STRING = [ MAJOR, MINOR, TINY, BUILD ].compact.join( "." )
-
-    VERSION = ::SQLite3::VERSION
-  end
-
-  def self.const_missing(name)
-    return super unless name == :Version
-    warn(<<-eowarn) if $VERBOSE
-#{caller[0]}: `SQLite::Version` will be removed in sqlite3-ruby version 2.0.0
-    eowarn
-    VersionProxy
-  end
 end
