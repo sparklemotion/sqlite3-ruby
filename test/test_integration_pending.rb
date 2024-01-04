06086b731ead244d630551035b63c7491b848054
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-05 00:30:50 +0100

    Remove test that busy_timeout holds GVL

--- test/test_integration_pending.rb
+++ test/test_integration_pending.rb
@@ -80,44 +80,6 @@ class TC_Integration_Pending < SQLite3::TestCase
     assert time.real*1000 >= 1000
   end
 
-  def test_busy_timeout_holds_gvl
-    work = []
-    Thread.new do
-      while true
-        sleep 0.1
-        work << '.'
-      end
-    end
-    sleep 1
-
-    @db.busy_timeout 1000
-    busy = Mutex.new
-    busy.lock
-
-    t = Thread.new do
-      begin
-        db2 = SQLite3::Database.open( "test.db" )
-        db2.transaction( :exclusive ) do
-          busy.lock
-        end
-      ensure
-        db2.close if db2
-      end
-    end
-    sleep 1
-
-    assert_raises( SQLite3::BusyException ) do
-      work << '>'
-      @db.execute "insert into foo (b) values ( 'from 2' )"
-    end
-
-    busy.unlock
-    t.join
-
-    p ['busy_timeout', work]
-    assert 2 >= work.size - work.find_index(">")
-  end
-
   def test_busy_handler_timeout_releases_gvl
     work = []
 
@@ -155,14 +117,13 @@ class TC_Integration_Pending < SQLite3::TestCase
     sleep 1
 
     assert_raises( SQLite3::BusyException ) do
-      work << '>'
+      work << '|'
       @db.execute "insert into foo (b) values ( 'from 2' )"
     end
 
     busy.unlock
     t.join
 
-    p ['busy_handler_timeout', work]
-    assert 2 < work.size - work.find_index(">")
+    assert work.size - work.find_index('|') > 3
   end
 end
